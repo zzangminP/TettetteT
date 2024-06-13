ca40af230e6e857e741cbfeaf7d4f60736389888
zzangmin <wjdals1212gh@gmail.com>
2024-06-13 18:00:20 +0900

    20240613

--- Assets/3.Script/Game/Board.cs
+++ Assets/3.Script/Game/Board.cs
@@ -11,7 +11,7 @@ public class Board : MonoBehaviour
     //public Board board;
 
     
-    public static RectInt bounds
+    public RectInt Bounds
     {
         get
         {
@@ -26,6 +26,7 @@ public class Board : MonoBehaviour
     {
         tilemap = GetComponentInChildren<Tilemap>();
         activePiece = GetComponentInChildren<Piece>();
+        Bounds bounds = new Bounds();
         //this.tetrominoes = GetComponent<TetrominoData>();
         //this.board = GetComponent<Board>();
         //테트로미노 생성
@@ -42,6 +43,9 @@ public class Board : MonoBehaviour
     }
 
     //테트로미노 데이터 중 랜덤으로 스폰
+
+
+    /*
     public void SpawnPiece()
     {
         //spawnPosition = new Vector3Int(0, 0, 0);
@@ -52,6 +56,24 @@ public class Board : MonoBehaviour
 
         Set(activePiece);
 
+    }*/
+
+
+    public void SpawnPiece()
+    {
+        int random = Random.Range(0, tetrominoes.Length);
+        TetrominoData data = tetrominoes[random];
+
+        activePiece.Initialize(this, spawnPosition, data);
+
+        if (IsValidPosition(activePiece, spawnPosition))
+        {
+            Set(activePiece);
+        }
+        else
+        {
+            //GameOver();
+        }
     }
 
 
@@ -65,16 +87,28 @@ public class Board : MonoBehaviour
         }
     }
 
-
-    private bool IsValidPosition(Piece piece, Vector2Int position)
+    //움직일 수 있는지 확인
+    public bool IsValidPosition(Piece piece, Vector3Int position)
     {
-        RectInt bounds;
-        for(int i = 0; i < piece.cells.Length; i++)
-        {
-            Vector3Int tilePosition = piece.cells[i] + piece.position;
-        }
-        //if()
+        RectInt bounds = Bounds;
 
+        // The position is only valid if every cell is valid
+        for (int i = 0; i < piece.cells.Length; i++)
+        {
+            Vector3Int tilePosition = piece.cells[i] + position;
+
+            // An out of bounds tile is invalid
+            if (!bounds.Contains((Vector2Int)tilePosition))
+            {
+                return false;
+            }
+
+            // A tile already occupies the position, thus invalid
+            if (tilemap.HasTile(tilePosition))
+            {
+                return false;
+            }
+        }
 
         return true;
     }
--- Assets/3.Script/Game/Piece.cs
+++ Assets/3.Script/Game/Piece.cs
@@ -8,16 +8,27 @@ public class Piece : MonoBehaviour
     public Vector3Int[] cells { get; private set; }
     public Vector3Int position { get; private set; }
 
+    
 
+
+    private float lockTime;
+    private float stepTime;
+    private float moveTime;
+    //
     private void Start()
     {
-
+        
     }
+
+    //private void U
     public void Initialize(Board board, Vector3Int position, TetrominoData data )
     {
         this.board = board;
         this.position = position;
         this.data = data;
+
+        //stepTime = Time.time;
+        //delayTime = Time.time - Time.deltaTime;
 
         if(this.cells == null)
         {
@@ -54,16 +65,115 @@ public class Piece : MonoBehaviour
         //    //transform.position += Vector3.up;
         //}
     }
-*/
-    private void Move(Vector2Int translation)
+
+    */
+    private bool Move(Vector2Int translation)
     {
-        
-        Vector3Int newPosition = new Vector3Int(0, 0, 0);
+        Vector3Int newPosition = position;
         newPosition.x += translation.x;
         newPosition.y += translation.y;
 
+        bool valid = board.IsValidPosition(this, newPosition);
 
+        
+        // Only save the movement if the new position is valid
+        if (valid)
+        {
+            position = newPosition;
+            moveTime = Time.time + Time.deltaTime;
+            lockTime = 0f; // reset
+        }
 
+        return valid;
     }
-    
+
+
+    private void Update()
+    {
+        //board.Clear(this);
+
+        // We use a timer to allow the player to make adjustments to the piece
+        // before it locks in place
+        lockTime += Time.deltaTime;
+        /*
+        // Handle rotation
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            Rotate(-1);
+        }
+        else if (Input.GetKeyDown(KeyCode.E))
+        {
+            Rotate(1);
+        }
+
+        // Handle hard drop
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            HardDrop();
+        }*/
+
+        // Allow the player to hold movement keys but only after a move delay
+        // so it does not move too fast
+        if (Time.time > moveTime)
+        {
+            HandleMoveInputs();
+        }
+
+        /*// Advance the piece to the next row every x seconds
+        if (Time.time > stepTime)
+        {
+            Step();
+        }
+
+        board.Set(this);*/
+    }
+
+    private void HandleMoveInputs()
+    {
+        // Soft drop movement
+        if (Input.GetKey(KeyCode.S))
+        {
+            if (Move(Vector2Int.down))
+            {
+                // Update the step time to prevent double movement
+                stepTime = Time.time + Time.deltaTime;//stepDelay;
+                //if()
+            }
+        }
+
+        // Left/right movement
+        if (Input.GetKey(KeyCode.A))
+        {
+            Move(Vector2Int.left);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            Move(Vector2Int.right);
+        }
+    }
+
+    /*
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+    }*/
+
+
+
+
+
+
+    /*
+    private bool IsBorder()
+    {
+        if (this.position.x == board.tilemap.localBounds.max.x ||
+            this.position.x == board.tilemap.localBounds.min.x ||
+            )
+        {
+            return true;
+        }
+        else
+            return false;
+    }
+    */
 }
