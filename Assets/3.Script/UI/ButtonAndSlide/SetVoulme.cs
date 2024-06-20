d7b6948846437740613311dec8d6988a3b65288d
zzangmin <38036544+zzangminP@users.noreply.github.com>
2024-06-20 22:04:29 +0900

    20240620_2
    Added GameClearScene

--- Assets/3.Script/UI/ButtonAndSlide/SetVoulme.cs
+++ Assets/3.Script/UI/ButtonAndSlide/SetVoulme.cs
@@ -12,18 +12,28 @@ public class SetVoulme : MonoBehaviour
     public void Setlevel(float sliderValue)
     {
 
-        if(sliderValue <=0.003)
+        if (sliderValue <= 0.003)
         {
             mixer.SetFloat("MusicVol", -80f);
         }
         else
         {
-            mixer.SetFloat("MusicVol", Mathf.Log10(sliderValue) * 20);
+            float temp;
+            mixer.GetFloat("MusicVol", out temp);
+            if (temp < Mathf.Log10((float)0.2) * 20)
+            {
+                mixer.SetFloat("MusicVol", (float)0.001);
+            }
+            else
+            {
+                mixer.SetFloat("MusicVol", Mathf.Log10(sliderValue) * 20);
+
+            }
 
         }
-            
 
-        
+
+
     }
 
 }
---
+++ Assets/3.Script/UI/etc/PRTT.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using DG.Tweening;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class PRTT : MonoBehaviour
+{
+
+    public TextMeshProUGUI tmpText;
+    public string fulltext = "Press \"R\" to Main Manu";
+    public float typingSpeed = 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(ShowText());
+    }
+
+    IEnumerator ShowText()
+    {
+
+        yield return new WaitForSeconds(2f);
+        tmpText.text = "";
+
+        foreach (char c in fulltext)
+        {
+            tmpText.text += c;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.R))
+        {
+            SceneManager.LoadScene("Title");
+        }
+    }
+}
